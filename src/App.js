cf53d04259ed035a42cc03a262b2bf5f14fe74c2
Alan Porcel <porcelalan@gmail.com>
2020-10-23 13:59:08 -0300

    Enviando counter

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/ItemCount';
-
 function App() {
   return (
     <div className="App">
       
         <NavBar />
+        <NavBar />
         <ItemListContainer title="MI LISTA DE PRODUCTOS" />
         <ItemCount stock="5" />
     </div>
 
-    
   );
 }
 
